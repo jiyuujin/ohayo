8f1d3c98c1e309ff809a1081e9a88d3cb9754fca
jiyuujin <jiyuujinunite@gmail.com>
2021-05-15 22:11:40 +0900

    feat: fetch test

---
+++ src/main.ts
@@ -0,0 +1,50 @@
+const { Client, LogLevel } = require('@notionhq/client');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const notion = new Client({
+    auth: process.env.NOTION_API_KEY,
+    logLevel: LogLevel.DEBUG
+});
+
+(async () => {
+    const fetchOhayoNote = async () => {
+        const response = await notion.databases.query({
+            database_id: process.env.OHAYO_NOTES_DATABASE_ID
+        });
+        console.log(response);
+    }
+
+    const createOhayoNote = async () => {
+        const current = new Date();
+        const response = await notion.pages.create({
+            parent: {
+                database_id: process.env.OHAYO_NOTES_DATABASE_ID,
+            },
+            properties: {
+                'Name': {
+                    title: [
+                        {
+                            text: {
+                                content: `${current.getMonth() + 1}/${current.getDate()} 予定`
+                            }
+                        }
+                    ]
+                }
+            }
+        });
+        console.log(response);
+    }
+
+    switch (process.argv[2]) {
+        case '1':
+            await fetchOhayoNote();
+            break;
+        case '2':
+            await createOhayoNote();
+            break;
+        default:
+            break;
+    }
+})();
