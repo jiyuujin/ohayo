9ef2b5c95851889f5c4088717094a5c0cc66b31c
jiyuujin <jiyuujinunite@gmail.com>
2021-11-08 21:47:34 +0900

    feat: backup - resolved #3

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 const { Client, LogLevel } = require('@notionhq/client');
-const dotenv = require('dotenv');
+const { Octokit } = require('@octokit/rest');
 
 const {
     getJPStandardDateTime,
@@ -7,11 +7,15 @@ const {
     getMaximumIndex
 } = require('./utils');
 
-dotenv.config();
+const dotenv = require('dotenv').config();
 
 const notion = new Client({
     auth: process.env.NOTION_API_KEY,
     logLevel: LogLevel.DEBUG
+});
+
+const octokit = new Octokit({
+    auth: dotenv.parsed.GITHUB_TOKEN
 });
 
 (async () => {
@@ -56,12 +60,61 @@ const notion = new Client({
         });
     }
 
+    const migrateOhayoNote = async () => {
+        const notes = await fetchOhayoNote();
+        for (const note of notes.results) {
+            let index = 1;
+            let content = ''
+            content += `---\n`
+            content += `title: ${note.properties['Name']?.title[0]?.plain_text}\n`
+            content += `event: \n`
+            note.properties['Event']?.multi_select?.forEach((event) => {
+                content += `  - ${event.name}\n`
+            })
+            let tagText = ''
+            let tagList = []
+            content += `tags:\n`
+            note.properties['Tags']?.multi_select?.forEach((tag, key) => {
+                content += `  - ${tag.name}\n`
+                tagText += tag.name
+                if (key !== note.properties['Tags']?.multi_select?.length - 1) {
+                    tagText += ','
+                }
+                tagList.push(tag.name)
+            })
+            content += `---\n\n`
+            const page = await notion.blocks.children.list({ block_id: note.id });
+            page.results.forEach((result) => {
+                result.bulleted_list_item?.text.forEach((item) => {
+                    content += `${item.plain_text}\n`;
+                });
+            });
+
+            octokit.issues.create({
+                owner: 'jiyuujin',
+                repo: 'ohayo-data', // repository name, ex: ohayo-data
+                title: note.properties['Name']?.title[0]?.plain_text,
+                body: content,
+                labels: tagList
+            }).then(({data}) => {
+                // console.log(data);
+            }).catch(error => {
+                console.log(error);
+            });
+
+            index++;
+        }
+    }
+
     switch (process.argv[2]) {
         case '--fetch':
             await fetchOhayoNote();
             break;
         case '--create':
             await createOhayoNote();
+            break;
+        case '--migrate':
+            await migrateOhayoNote();
             break;
         default:
             break;
